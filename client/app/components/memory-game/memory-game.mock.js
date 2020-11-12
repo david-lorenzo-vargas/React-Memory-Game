1830b37fbcd99f2c2dadfe01ec8b9146447480a4
unknown <dlorenzo.05@gmail.com>
2020-11-12 22:02:53 +0100

    memory-game.mock.js file created with an array ob ojects, each object is an image with name, id and url keys

---
+++ client/app/components/memory-game/memory-game.mock.js
@@ -0,0 +1,54 @@
+const images = [
+  {
+    name: 'ice cream',
+    id: 1,
+    url: 'https://images.unsplash.com/photo-1505394033641-40c6ad1178d7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1406&q=80',
+  },
+  {
+    name: 'meat',
+    id: 2,
+    url: 'https://images.unsplash.com/photo-1575229228428-8779b8a883a1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=80',
+  },
+  {
+    name: 'flower',
+    id: 3,
+    url: 'https://images.unsplash.com/photo-1502977249166-824b3a8a4d6d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=80',
+  },
+  {
+    name: 'zebra',
+    id: 4,
+    url: 'https://images.unsplash.com/photo-1501706362039-c06b2d715385?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=535&q=80',
+  },
+  {
+    name: 'sunflower',
+    id: 5,
+    url: 'https://images.unsplash.com/photo-1603973684108-e712f8652a39?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=80',
+  },
+  {
+    name: 'thailand',
+    id: 6,
+    url: 'https://images.unsplash.com/photo-1492693377442-7f88103dc502?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=80',
+  },
+  {
+    name: 'temple',
+    id: 7,
+    url: 'https://images.unsplash.com/photo-1569155829632-148eea0844af?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=479&q=80',
+  },
+  {
+    name: 'cloud',
+    id: 8,
+    url: 'https://images.unsplash.com/photo-1573630309736-1f2bd189b509?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=80',
+  },
+  {
+    name: 'plane',
+    id: 9,
+    url: 'https://images.unsplash.com/photo-1496054682147-258414043211?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=80',
+  },
+  {
+    name: 'volcano',
+    id: 10,
+    url: 'https://images.unsplash.com/photo-1574786199452-c7a5f69fb6e9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=80',
+  },
+];
+
+export default images;
