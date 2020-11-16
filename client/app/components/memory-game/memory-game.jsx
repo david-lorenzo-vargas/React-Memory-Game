81d570a3321a17dbad2c73de1af7f92d5444c04c
unknown <dlorenzo.05@gmail.com>
2020-11-17 00:26:51 +0100

    new arrays of images imported, imageArray key added to the state that will contain the array that will used to play, all the new functions bind, logic added in shuffleCards to shuffle the array that will receive from state depending on the button that we click, new button created in render to choose the array to play with, functions created for each button to set the state with the specific array o images and to start the game

--- client/app/components/memory-game/memory-game.jsx
+++ client/app/components/memory-game/memory-game.jsx
@@ -2,6 +2,9 @@ import React from 'react';
 import { Row, Column } from '../grid';
 import Card from '../card';
 import images from './memory-game.mock';
+import animalImages from './memory-game-animals.mock';
+import foodImages from './memory-game-food.mock';
+import travelImages from './memory-game-travel.mock';
 import Button from '../button';
 import Text from '../text';
 import styles from './memory-game.scss';
@@ -11,6 +14,7 @@ class MemoryGame extends React.Component {
     super(props);
 
     this.state = {
+      imageArray: [],
       currentCard: '',
       currentId: 0,
       previousId: 0,
@@ -21,14 +25,18 @@ class MemoryGame extends React.Component {
 
     this.handleCardClick = this.handleCardClick.bind(this);
     this.handlePlayAgainButtonClick = this.handlePlayAgainButtonClick.bind(this);
-    this.handleStartButtonClick = this.handleStartButtonClick.bind(this);
+    this.handleTravelButtonClick = this.handleTravelButtonClick.bind(this);
     this.handleBackButtonClick = this.handleBackButtonClick.bind(this);
+    this.handleMiscelaneusButtonClick = this.handleMiscelaneusButtonClick.bind(this);
+    this.handleAnimalButtonClick = this.handleAnimalButtonClick.bind(this);
+    this.handleFoodButtonClick = this.handleFoodButtonClick.bind(this);
   }
 
   //Fisher–Yates shuffle algorithm =================
   //==================================
   shuffleCards() {
-    const array = images;
+    const { imageArray } = this.state;
+    const array = imageArray;
     for (let i = array.length - 1; i > 0; i--) {
       const j = Math.floor(Math.random() * (i + 1));
       [array[i], array[j]] = [array[j], array[i]];
@@ -105,9 +113,31 @@ class MemoryGame extends React.Component {
     });
   }
 
-  handleStartButtonClick() {
+  handleTravelButtonClick() {
     this.shuffleCards();
     this.setState({
+      imageArray: travelImages,
+      gameStarted: true,
+    })
+  }
+
+  handleMiscelaneusButtonClick() {
+    this.setState({
+      imageArray: images,
+      gameStarted: true,
+    })
+  }
+
+  handleAnimalButtonClick() {
+    this.setState({
+      imageArray: animalImages,
+      gameStarted: true,
+    })
+  }
+
+  handleFoodButtonClick() {
+    this.setState({
+      imageArray: foodImages,
       gameStarted: true,
     })
   }
@@ -127,23 +157,57 @@ class MemoryGame extends React.Component {
   render() {
     console.log(this.state);
 
-    const { currentId, previousId, gameStarted } = this.state;
+    const { currentId, previousId, gameStarted, imageArray } = this.state;
 
     return (
       <div className={styles['memory-game']}>
         <Text text="MEMORY GAME" theme="blue" size="big" />
         {!gameStarted ?
+        <>
           <Row>
-          <Button
-            text="START"
-            theme="blue"
-            size="medium"
-            onClick={this.handleStartButtonClick}
-          />
-          </Row> :
+            <div className={styles['memory-game__item']}>
+              <Button
+                text="TRAVEL"
+                theme="blue"
+                size="medium"
+                onClick={this.handleTravelButtonClick}
+              />
+            </div>
+          </Row>
+          <Row>
+            <div className={styles['memory-game__item']}>
+              <Button
+                text="ANIMALS"
+                theme="blue"
+                size="medium"
+                onClick={this.handleAnimalButtonClick}
+              />
+            </div>
+          </Row>
+          <Row>
+            <div className={styles['memory-game__item']}>
+              <Button
+                text="FOOD"
+                theme="blue"
+                size="medium"
+                onClick={this.handleFoodButtonClick}
+              />
+            </div>
+          </Row>
+          <Row>
+            <div className={styles['memory-game__item']}>
+              <Button
+                text="MISCELLANEOUS"
+                theme="blue"
+                size="medium"
+                onClick={this.handleMiscelaneusButtonClick}
+              />
+            </div>
+          </Row>
+        </> :
           <>
             <Row>
-              {images.map((item) => (
+              {imageArray.map((item) => (
                 <>
                   <Column>
                     <div className={styles['memory-game__item']}>
@@ -161,29 +225,32 @@ class MemoryGame extends React.Component {
                 </>
               ))}
             </Row>
-            <Row>
-            <Column>
-              <div className={styles['memory-game__item']}>
-                <Button
-                  text="BACK"
-                  theme="blue"
-                  size="medium"
-                  onClick={this.handleBackButtonClick}
-                />
-              </div>
-              </Column>
-              <Column>
-                <div className={styles['memory-game__item']}>
-                  <Button
-                  text="PLAY AGAIN"
-                  theme="blue"
-                  size="medium"
-                  onClick={this.handlePlayAgainButtonClick}
-                  />
-                </div>
-              </Column>
-          </Row>
-        </>}
+            <>
+              <Row>
+                <Column>
+                    <div className={styles['memory-game__item']}>
+                      <Button
+                        text="BACK"
+                        theme="blue"
+                        size="medium"
+                        onClick={this.handleBackButtonClick}
+                      />
+                    </div>
+                  </Column>
+                  <Column>
+                    <div className={styles['memory-game__item']}>
+                      <Button
+                      text="PLAY AGAIN"
+                      theme="blue"
+                      size="medium"
+                      onClick={this.handlePlayAgainButtonClick}
+                      />
+                    </div>
+                  </Column>
+              </Row>
+            </>
+          </>
+        }
       </div>
     );
   }
